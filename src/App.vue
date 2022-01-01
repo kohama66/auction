ea5ac8a572f18752b1cac38c9fc12bdb0c2574cf
kohama66 <54892265+kohama66@users.noreply.github.com>
2022-01-01 22:50:14 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,14 @@ export default defineComponent({
 </script>
 
 <style>
-@import 'assets/css/reset.css';
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "assets/css/reset.css";
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-} */
+  margin-top: 60px; */
+  box-sizing: border-box;
+}
 </style>
