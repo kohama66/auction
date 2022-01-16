01a5b08850b0734e24db152335f9a596c42d3228
kohama66 <54892265+kohama66@users.noreply.github.com>
2022-01-16 20:49:54 +0900

    add component

---
+++ src/components/atoms/BaseText.vue
@@ -0,0 +1,44 @@
+<template>
+  <p class="base-text">
+    {{ state.text }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+type State = {
+  text: string;
+};
+export default defineComponent({
+  name: "BaseText",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 16,
+    },
+  },
+
+  setup(props) {
+    const state = reactive<State>({
+      text: props.text,
+    });
+    const fontSize = `${props.size}px`;
+
+    return {
+      state,
+      fontSize,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.base-text {
+  font-size: v-bind(fontSize);
+}
+</style>
